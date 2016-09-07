39f65f44e4feca8c03027fa505294e9a8ae04f6c
GaoHawk <hawk13971263496@gmail.com>
2016-09-07 16:43:02 +0800

    complete

--- webgl/js/ClickedPoints.js
+++ webgl/js/ClickedPoints.js
@@ -2,7 +2,7 @@
 * @Author: Administrator
 * @Date:   2016-09-07 15:18:14
 * @Last Modified by:   Administrator
-* @Last Modified time: 2016-09-07 15:31:36
+* @Last Modified time: 2016-09-07 16:41:50
 */
 
 'use strict';
@@ -18,10 +18,9 @@
 //  顶点着色器程序
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
-    'attribute float a_PointSize;\n' +
     'void main() {\n' +
     ' gl_Position = a_Position;\n' + // 设置坐标
-    ' gl_PointSize = a_PointSize;\n' +   // 设置尺寸
+    ' gl_PointSize = 10.0;\n' +   // 设置尺寸
     '}\n';
 
  // 片元着色器程序
@@ -46,12 +45,14 @@ function main(){
   }
   // 获取attribute变量的存储位置
   var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+  // var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
   if (a_Position < 0) {
       console.log('Failed to get the storage location of a_Position');
   }
   gl.vertexAttrib3f(a_Position,0.0,0.0,0.0);
-  gl.vertexAttrib1f(a_PointSize,5.0);
+
+  canvas.onmousedown = function(ev) {click(ev,gl,canvas,a_Position); };
+  // gl.vertexAttrib1f(a_PointSize,5.0);
 
   // 设置<canvas>背景色
   gl.clearColor(0.0,0.0,0.0,1.0);
@@ -59,4 +60,26 @@ function main(){
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   gl.drawArrays(gl.POINTS,0 ,1);
+}
+var g_points = [];
+function click(ev, gl, canvas, a_Position) {
+   var x = ev.clientX;
+   var y = ev.clientY;
+   var rect = ev.target.getBoundingClientRect();
+
+   x = ((x - rect.left) - canvas.height/2) / (canvas.height/2);
+    y = (canvas.width/2 - (y - rect.top))/(canvas.width/2);
+
+  // 将坐标存储到g_points数组中
+  g_points.push(x);g_points.push(y);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var len = g_points.length;
+  for(var i =0;i < len; i +=2){
+
+    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+
+    gl.drawArrays(gl.POINTS,0,1);
+  }
 }
